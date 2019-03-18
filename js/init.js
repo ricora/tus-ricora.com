10c11947c6f926c4694fd4371faaf10c1424b565
take <touhoucrisis7@gmail.com>
2019-03-18 13:15:40 +0900

    home page 2019 - 2

--- js/init.js
+++ js/init.js
@@ -19,13 +19,14 @@
 /*	gmaps
 ------------------------------------------------------*/
 
-   var map;
 
+   var map;
    // main directions
    map = new GMaps({
       el: '#map', lat: 14.553533, lng: 121.052124, zoom: 14, zoomControl : true,
       zoomControlOpt: { style : 'SMALL', position: 'TOP_LEFT' }, panControl : false, scrollwheel: false
    });
+   
 
    // add address markers
    map.addMarker({ lat: 14.552891, lng: 121.051815, title: 'Eco Tower',
